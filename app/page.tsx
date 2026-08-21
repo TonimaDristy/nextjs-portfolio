fe33ab68793254a40bc2613b02e6cac9d512c8ac
TonimaDristy <tidristy477@gmail.com>
2026-08-21 18:26:57 +0600

    Added Nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <div className="text-center">
-        <p className="mb-3 text-xl text-purple-400">Hi, I am</p>
+    <>
+      <Navbar />
 
-        <h1 className="text-5xl font-bold md:text-7xl">
-          Tonima Islam <span className="text-purple-500">Dristy</span>
-        </h1>
+      <main
+        id="home"
+        className="flex min-h-screen items-center justify-center bg-black text-white"
+      >
+        <div className="text-center">
+          <p className="mb-3 text-xl text-purple-400">Hi, I am</p>
 
-        <p className="mt-4 text-xl text-gray-400">My Next.js Portfolio</p>
-      </div>
-    </main>
+          <h1 className="text-5xl font-bold md:text-7xl">
+            Tonima Islam <span className="text-purple-500">Dristy</span>
+          </h1>
+
+          <p className="mt-4 text-xl text-gray-400">My Next.js Portfolio</p>
+        </div>
+      </main>
+    </>
   );
 }
