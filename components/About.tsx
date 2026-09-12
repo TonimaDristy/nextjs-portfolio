9670804a7e9e0502b1b6efd17555faa88060c48a
TonimaDristy <tidristy477@gmail.com>
2026-09-12 23:58:34 +0600

    Add new texts in skill tab

--- components/About.tsx
+++ components/About.tsx
@@ -119,6 +119,7 @@ export default function About() {
                   </h3>
                   <p className="mt-2 leading-7 text-gray-400">
                     Developed interactive 2D graphics and animated cityscape
+                    projects using C++ and OpenGL.
                   </p>
                 </div>
               </div>
